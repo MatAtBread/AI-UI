6a9807ad0bd48d3ef62e282dec8ba4a64b2d61ec
bread@mailed.me.uk <bread@mailed.me.uk>
2023-10-30 23:11:39 +0000

    More documentation & weather example

--- guide/examples/ts/weather.ts
+++ guide/examples/ts/weather.ts
@@ -1,57 +1,136 @@
 import { tag } from '../../../module/esm/ai-ui.js'
 //import { tag } from 'https://unpkg.com/@matatbread/ai-ui/esm/ai-ui.js'
 
-const { h2, div, button, select, option, img } = tag();
+const { div, img, input } = tag();
 
-/* Uses: 
+/* With thanks to https://open-meteo.com/
   Geocoding: https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=1&language=en&format=json
   Weather: https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m
 */
 
+interface GeoInfo {
+  id: number,
+  name: string,
+  latitude: number,
+  longitude: number,
+  elevation: number,
+  country_code: string,
+  timezone: `${string}/${string}`,
+  population: number,
+  country: string,
+}
+
+interface GeoInfoResponse {
+  results: GeoInfo[]
+}
+
+interface Forecast {
+  temperature_2m_max: number[]
+  time: number[]
+}
+
+async function getGeoInfo(s: string): Promise<GeoInfoResponse> {
+  return fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(s)}&count=1&language=en&format=json`)
+    .then(res => res.json())
+}
+
+async function getWeatherForecast(g: GeoInfo): Promise<Forecast> {
+  return fetch(`https://api.open-meteo.com/v1/forecast?forecast_days=16&timezone=${encodeURIComponent(g.timezone)}&latitude=${g.latitude}&longitude=${g.longitude}&daily=temperature_2m_max&timeformat=unixtime&format=json`)
+    .then(res => res.json())
+    .then(obj => obj.daily)
+}
+
+/* 
+  Define a "Chart" so it is like an image, but with additional attributes called `label`,
+  `xData` and `yData`. 
+
+  When these are all set, draw a chart for the data within the image
+*/
+
 const Chart = img.extended({
-  constructed() {
-  },
   prototype: {
     label: '',
-    x: [] as number[],
-    set y(d: number[]) {
-      this.src = "https://quickchart.io/chart?width=300&height=200&chart="+encodeURIComponent(JSON.stringify({
-        type:'bar',
-        data:{
-          labels: this.x,
-          datasets: [{
-            label: this.label,
-            data: d
-          }]
+    xData: [] as (string | number)[],
+    set yData(d: number[]) {
+      if (this.xData && this.label) {
+        this.src = `https://quickchart.io/chart?width=${this.width}&height=${this.height}&chart=` + encodeURIComponent(JSON.stringify({
+          type: 'line',
+          data: {
+            labels: this.xData,
+            datasets: [{
+              label: this.label,
+              data: d
+            }]
+          }
+        }))
       }
-    }))
+    }
   }
-  }
-})
+});
 
-function sleep(seconds: number) {
-  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-}
+/* Define a "Location" element that is like an input tag that defaults to 'block' display style,
+  and can indicate errors in a predefined way */
+const Location = input.extended({
+  prototype: {
+    placeholder: 'Enter a town...',
+    style: {
+      display: 'block'
+    },
+    showError(f: boolean) {
+      (this.style as any).backgroundColor = f ? '#fdd' : '';
+    },
+    onkeydown() {
+      this.showError(false);
+    }
 
-async function *synthData() {
-  while (true) {
-    yield [Math.random(),Math.random(),Math.random(),Math.random(),Math.random()]
-    await sleep(3);
   }
-}
+});
 
 const App = div.extended({
+  ids:{
+    weather: Chart
+  },
   async constructed() {
-    // When we're constructed, create a few children within the element by default */
+    /* When we're constructed, create a few children within the element by default.
+      We also keep a reference to tha thing we're creating as we're using it in 
+      am event handler. This is the common way to do this in DOM code, but is better 
+      handled using `when`.
+    */
+    const app = this;
     return [
+      Location({
+        async onblur(e) {
+          /* Note: this is the "obvious" way to do this (set the chart when the event
+            fires), however AI-UI provides the `when` mechanism to make this much
+            simpler, cleaner, type-safe and, most usefully, separating the data & events from
+            the UI and layout, so that our "Location" tag doesn't have to ASSUME the 
+            nextElementSibling is a Chart. 
+            
+            However, being just normal DOM elements, we choose to do it the "obvious" way 
+            to introduce new concepts in an appropriate place.
+            
+            See https://github.com/MatAtBread/AI-UI/blob/main/guide/when.md
+          */
+          try {
+            const geo = await getGeoInfo(this.value);
+            const forecast = await getWeatherForecast(geo.results[0]);
+
+            app.ids.weather!.label = geo.results[0].name;
+            app.ids.weather!.xData = forecast.time.map(t => new Date().toDateString());
+            app.ids.weather!.yData = forecast.temperature_2m_max;
+          } catch (ex) {
+            this.showError(true);
+          }
+        }
+      }),
       Chart({
-        label: 'Test',
-        x: [1,2,3,4,5],
-        y: [9,4,7,2,3]//synthData()
+        id: 'weather',
+        width: 600,
+        height: 400
       })
     ]
   }
 });
 
-/* Create and add an "App" element to the document so the user can see it! */
+/* Create and add the "App" element to the document so the user can see it! */
 document.body.appendChild(App());
--- guide/extended.md
+++ guide/extended.md
@@ -55,17 +55,13 @@ TagFunctionName.extended((privateInstanceData: MyPrivateInstance) => {
 
 # Example
 
-In order to explain how these four members interact and support each other, we're going to consider a new a example, web page that gets weather data from a remote source, creates charts for the user, and a QR codes that represent the chart that can be shared.
+In order to explain how these four members interact and support each other, we're going to consider a new a example, web page that gets weather data from a remote source and creates charts for the user.
 
-We're going to do this by creating tags that encapsulate the data source, the chart UI and the QR cods, and show how we can statically layout and style the page, whilst ensuring the page remains dynamically updated according to user input.
+We're going to do this by creating tags that encapsulate the data source and the chart UI, and show how we can statically layout and style the page, whilst ensuring the page remains dynamically updated according to user input.
 
-First, here is the weather:
+You can see the source [here](./examples/ts/weather.ts), or try it [here](https://raw.githack.com/MatAtBread/AI-UI/0.9.11/guide/examples/ts/ts-example.html#weather.ts)
 
-```typescript
-
-```
-
-[Example (right click and open in new tab)](https://raw.githack.com/MatAtBread/AI-UI/0.9.11/guide/examples/ts/ts-example.html#weather.ts)
+in the following sections, we'll go through the code in more detail, explaining how the four members of an `extended({...})` definition work together, improving the implementation on the way.
 
 ____
 
