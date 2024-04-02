550ab85d7f333595b8b6b41a031f3b4e27146987
bread@mailed.me.uk <bread@mailed.me.uk>
2024-04-02 02:45:14 +0100

    Mved test code to test file

--- module/esm/tags.js
+++ module/esm/tags.js
@@ -2,39 +2,3 @@
   No code/data is declared in this file (except the re-exported symbols from iterators.ts).
 */
 export const UniqueID = Symbol("Unique ID");
-// declare const div: TagCreator<HTMLDivElement>;
-// const I = div.extended(inst => ({
-//   iterable:{
-//     i: 0
-//   },
-//   constructed(){
-//     const x = this.i.map!(i => i+1);
-//     return Symbol('x');
-//   }
-// }))
-// const J = div.extended(inst => ({
-//   iterable:{
-//     i: 0
-//   },
-//   constructed(){
-//     const x = this.i.map!(i => i+1);
-//     return x;
-//   }
-// }))
-// const K = div.extended(inst => ({
-//   iterable:{
-//     i: 0
-//   },
-//   constructed(){
-//     const x = this.i.map!(i => i+1);
-//   }
-// }))
-// const L = div.extended(inst => ({
-//   iterable:{
-//     i: 0
-//   }
-// }))
-// I()
-// J()
-// K()
-// L()
